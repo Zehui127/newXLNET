2b759688352fdc992d59da7d97f40cb7c08c00c1
Zehui <zy18763@nottingham.edu.cn>
2019-12-30 17:13:21 +0000

    instruction

--- scripts/tpu_squad_large.sh
+++ scripts/tpu_squad_large.sh
@@ -11,13 +11,13 @@ GS_MODEL_DIR=${GS_ROOT}/experiment/squad
 
 # TPU name in google cloud
 
-python3 run_squad.py \
+python run_squad.py \
   --use_tpu=True \
   --tpu=${TPU_NAME} \
   --num_hosts=1 \
   --num_core_per_host=8 \
-  --model_config_path=gs://run_bert_tpu/xlnet/xlnet_cased_L-12_H-768_A-12/xlnet_config.json \
-  --spiece_model_file=gs://run_bert_tpu/xlnet/xlnet_cased_L-12_H-768_A-12/spiece.model \
+  --model_config_path=${SQUAD_DIR}/xlnet_config.json \
+  --spiece_model_file=${SQUAD_DIR}/spiece.model \
   --output_dir=${GS_PROC_DATA_DIR} \
   --init_checkpoint=${INIT_CKPT_DIR}/xlnet_model.ckpt \
   --model_dir=${GS_MODEL_DIR} \
@@ -25,7 +25,7 @@ python3 run_squad.py \
   --predict_file=${SQUAD_DIR}/dev-v2.0.json \
   --uncased=False \
   --max_seq_length=512 \
-  --do_train=True \
+  --do_train=False \
   --train_batch_size=48 \
   --do_predict=True \
   --predict_batch_size=32 \
@@ -35,4 +35,22 @@ python3 run_squad.py \
   --save_steps=1000 \
   --train_steps=8000 \
   --warmup_steps=1000 \
+  $@
+
+
+#### local path
+SQUAD_DIR=SQUAD
+
+#### google storage path
+GS_ROOT=gs://run_bert_tpu/xlnet
+GS_PROC_DATA_DIR=${GS_ROOT}/proc_data/squad
+
+python run_squad.py \
+  --use_tpu=False \
+  --do_prepro=True \
+  --spiece_model_file=${SQUAD_DIR}/spiece.model \
+  --train_file=${SQUAD_DIR}/train-v2.0.json \
+  --output_dir=${GS_PROC_DATA_DIR} \
+  --uncased=False \
+  --max_seq_length=512 \
   $@
